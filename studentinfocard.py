ca134cc8f078714754f9079170bcebe52a921ed4
Ritisha Juneja <ritishajuneja26@gmail.com>
2026-05-21 01:08:20 +0530

    Create studentinfocard.py
    # Python Beginner Projects
    
    These are my first beginner Python mini projects made while learning Python fundamentals.
    
    ## Projects Included
    1. Student Info Card Generator
    2. Simple Calculator
    
    ## Concepts Used
    - Variables
    - Data Types
    - Input/Output
    - Operators
    - Typecasting
    
    More projects coming soon :)

---
+++ studentinfocard.py
@@ -0,0 +1,18 @@
+# Enter your name: Ritisha
+# Enter your age: 18
+# Enter your college: CU
+# Enter your course: CSE AI/ML
+# Enter your dream company: Google
+
+name=input("Enter your name: ")
+age=input("Enter your age: ")
+clg=input(" Enter your college: ")
+course=input("Enter your course:")
+com=input("Enter your dream company: ")
+print("STUDENT INFO CARD GENERATOR")
+print(name)
+print("OMGGGG!!! nice name",name)
+print(age)
+print(clg)
+print(course)
+print(com)
